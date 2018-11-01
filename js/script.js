391947c5c2a0c2e3d85b27fef19ce53d46a67459
AleksandrVais <Aleksandr.vais18@gmail.com>
2018-11-01 12:35:50 +0300

    Added Slick slider (sourse http://kenwheeler.github.io/slick/)But i need modify this slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+// Slick slider
 
+ $(document).ready(function(){
+  $('.rev_slider').slick({
+    slidesToShow: 1,
+  	slidesToScroll: 1,
+  	arrows: false,
+  	fade: true,
+  	asNavFor: '.rev_slider_nav'
+  });
+  $('.rev_slider_nav').slick({
+  	 // autoplay: true,
+  	  infinite: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 0,
+	  asNavFor: '.rev_slider',
+	  centerMode: true,
+	  focusOnSelect: true
+});
+}); 
